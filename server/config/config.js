edbbaf075690a3fcfa646fb851ac72659758a6fe
Nicolas Dona <nico.01.06.93@gmail.com>
2019-02-26 16:37:52 -0300

    process.env.MONGO_URL

--- server/config/config.js
+++ server/config/config.js
@@ -19,7 +19,7 @@ let urlDb;
 if(process.env.NODE_ENV == 'dev') {
     urlDb = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDb = 'mongodb+srv://nico:6R1OPN8naFkSaTUZ@cluster-fotah.mongodb.net/test?retryWrites=true';
+    urlDb = process.env.MONGO_URL;
 }
 
 process.env.URL_DB = urlDb;
